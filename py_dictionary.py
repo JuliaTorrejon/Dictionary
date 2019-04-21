46c1880717e210e27a927df0a7c82d7ea1b298e3
Julia Torrejon <juliaesther.torrejon@gmail.com>
2019-04-21 19:50:54 +0100

    Create py_dictionary.py

---
+++ py_dictionary.py
@@ -0,0 +1,10 @@
+from PyDictionary import PyDictionary
+
+dictionary=PyDictionary()
+features="html.parser"
+
+print (dictionary.meaning(""))
+
+print (dictionary.synonym(""))
+
+print (dictionary.antonym(""))
